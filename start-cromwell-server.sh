f081edd7e82469e13a68b7d12577027c9e5d49c0
hongiiv <hongiiv@gmail.com>
2019-07-11 10:13:39 +0900

    docker-compose

--- start-cromwell-server.sh
+++ start-cromwell-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd /opt/cromwell
-java -Dconfig.file=google.conf -Dsystem.abort-jobs-on-terminate=true -jar cromwell-43.jar server
+java -Dconfig.file=google.conf -jar cromwell-43.jar server
