2f010dff0754c3fdc93d1a7e2d91d3af1e5b2a38
hongiiv <hongiiv@gmail.com>
2019-07-11 09:23:57 +0900

    travis script

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ expected="200"
 actual=`wget --server-response http://localhost:8080/engine/v1/status 2>&1 | awk '/^  HTTP/{print $2}'`
 echo "Expecting: $expected"
 echo "Server says: $actual"
-if [ "$expected != "$actual ]; then
+if [ "$expected" != "$actual" ]; then
     echo "Test failed"
     exit 1
 else
