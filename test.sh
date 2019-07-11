5aa0d16b85428ffe5593c97add711a99dfee3586
hongiiv <hongiiv@gmail.com>
2019-07-11 09:06:39 +0900

    test code

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+expected="200"
+actual =`wget --server-response http://localhost:8080/engine/v1/status 2>&1 | awk '/^  HTTP/{print $2}'`
+echo "Expecting: $expected"
+echo "Server says: $actual"
+if [ "$expected != "$actual ]; then
+    echo "Test failed"
+    exit 1
+else
+    echo "Test passed"
+    exit 0
+fi
